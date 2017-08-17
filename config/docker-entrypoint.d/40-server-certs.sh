ef4c440db8fcdc93468dfd6b4b7835ffcdbeb48b
Petr Řehoř <rx@rx.cz>
2017-08-18 00:39:09 +0200

    Properly create CA certificate fingerprint link in /stc/ssl/certs

--- config/docker-entrypoint.d/40-server-certs.sh
+++ config/docker-entrypoint.d/40-server-certs.sh
@@ -99,10 +99,12 @@ fi
 ################################################################################
 
 # Create CA certificate fingerprint link to /etc/ssl/certs
-CA_CRT_HASH_FILE="/etc/ssl/certs/$(openssl x509 -hash -noout -in ${CA_CRT_FILE}).0"
-if [ -n "${CA_CRT_FILE}" -a -e "${CA_CRT_FILE}" -a ! -e "${CA_CRT_HASH_FILE}" ]; then
-  info "Creating CA certificate hash link server ${CA_CRT_HASH_FILE}"
-  ln -s ${CA_CRT_FILE} ${CA_CRT_HASH_FILE}
+if [ -n "${CA_CRT_FILE}" -a -e "${CA_CRT_FILE}" ]; then
+  CA_CRT_HASH_FILE="/etc/ssl/certs/$(openssl x509 -hash -noout -in ${CA_CRT_FILE}).0"
+  if [ ! -e "${CA_CRT_HASH_FILE}" ]; then
+    info "Creating CA certificate hash link server ${CA_CRT_HASH_FILE}"
+    ln -s ${CA_CRT_FILE} ${CA_CRT_HASH_FILE}
+  fi
 fi
 
 ################################################################################
