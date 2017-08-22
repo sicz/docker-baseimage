35bf39b59f63cefb9d688685ff649f2316872b97
Petr Řehoř <rx@rx.cz>
2017-08-22 20:25:28 +0200

    Improve tests

--- spec/docker/30_server_certs_spec.rb
+++ spec/docker/30_server_certs_spec.rb
@@ -23,13 +23,13 @@ describe "Server certificate", :test => :server_certs do
   describe x509_certificate(crt) do
     context "file" do
       subject { file(crt) }
-      it { is_expected.to be_a_file }
+      it { is_expected.to be_file }
       it { is_expected.to be_mode(644) }
       it { is_expected.to be_owned_by(user) }
       it { is_expected.to be_grouped_into(group) }
     end
     context "certificate" do
-      it { is_expected.to be_a_certificate }
+      it { is_expected.to be_certificate }
       it { is_expected.to be_valid }
     end
     its(:subject) { is_expected.to eq "/#{subj}" }
@@ -50,7 +50,7 @@ describe "Server certificate", :test => :server_certs do
   describe "X509 private key passphrase \"#{pwd}\"" do
     context "file" do
       subject { file(pwd) }
-      it { is_expected.to be_a_file }
+      it { is_expected.to be_file }
       it { is_expected.to be_mode(640) }
       # TODO: server_key.pwd is copied to container with strange owner
       # it { is_expected.to be_owned_by(user) }
@@ -63,7 +63,7 @@ describe "Server certificate", :test => :server_certs do
   describe x509_private_key(key, {:passin => "file:#{pwd}"}) do
     context "file" do
       subject { file(key) }
-      it { is_expected.to be_a_file }
+      it { is_expected.to be_file }
       it { is_expected.to be_mode(640) }
       it { is_expected.to be_owned_by(user) }
       it { is_expected.to be_grouped_into(group) }
@@ -82,7 +82,7 @@ describe "Server certificate", :test => :server_certs do
   describe "PKCS12 keystore \"#{p12}\"" do
     context "file" do
       subject { file(p12) }
-      it { is_expected.to be_a_file }
+      it { is_expected.to be_file }
       it { is_expected.to be_mode(640) }
       it { is_expected.to be_owned_by(user) }
       it { is_expected.to be_grouped_into(group) }
@@ -100,9 +100,10 @@ describe "Server certificate", :test => :server_certs do
         expect(subject.stderr).to match("PKCS7 Encrypted data: pbeWithSHA1And40BitRC2-CBC")
       end
     end
+    # TODO: Serverspec does not support PKCS12 keystores
     # context "certificate" do
-    #   it { is_expected.to be_a_certificate }
-    #   it { is_expected.to be_valid }
+    #   it { expect(subject.certificate).to be_certificate }
+    #   it { expect(subject.certificate).to be_valid_certificate }
     # end
     # its(:subject) { is_expected.to eq "/#{subj}" }
     # its(:issuer)  { is_expected.to eq "/CN=Docker Simple CA" }
@@ -117,9 +118,9 @@ describe "Server certificate", :test => :server_certs do
     # end
     context "key" do
       # TODO: Serverspec does not support PKCS12 keystores
-      # it { is_expected.to be_encrypted }
-      # it { is_expected.to be_valid }
-      # it { is_expected.to have_matching_certificate(crt) }
+      # it { expect(subject.key).to be_encrypted_key }
+      # it { expect(subject.key).to be_valid_key }
+      # it { expect(subject.key).to have_matching_certificate }
       subject { command("openssl pkcs12 -in #{p12} -passin file:#{pwd} -noout -info") }
       it "should be encrypted" do
         expect(subject.exit_status).to eq(0)
