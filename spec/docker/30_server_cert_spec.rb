b6063c0842528c11b8c93f520aeab0f8e16d568b
Petr Řehoř <rx@rx.cz>
2017-08-30 16:05:20 +0200

    Improve server_cert tests

--- spec/docker/30_server_cert_spec.rb
+++ spec/docker/30_server_cert_spec.rb
@@ -8,15 +8,23 @@ describe "Server certificate", :test => :server_cert do
 
   ### CONFIG ###################################################################
 
-  user = "root"
-  group = "root"
+  crt         = ENV["SERVER_CRT_FILE"]        || "/etc/ssl/certs/server.crt"
+  crt_subj    = ENV["SERVER_CRT_SUBJECT"]     || "CN=#{ENV["CONTAINER_NAME"]}"
+  crt_user    = ENV["SERVER_CRT_FILE_USER"]   || "root"
+  crt_group   = ENV["SERVER_CRT_FILE_GROUP"]  || "root"
+  crt_mode    = ENV["SERVER_CRT_FILE_MODE"]   || 444
 
-  crt = ENV["SERVER_CRT_FILE"]      || "/etc/ssl/certs/server.crt"
-  key = ENV["SERVER_KEY_FILE"]      || "/etc/ssl/private/server.key"
-  pwd = ENV["SERVER_KEY_PWD_FILE"]  || "/etc/ssl/private/server.pwd"
-  p12 = ENV["SERVER_P12_FILE"]      || "/etc/ssl/private/server.p12"
+  key         = ENV["SERVER_KEY_FILE"]        || "/etc/ssl/private/server.key"
+  key_pwd     = ENV["SERVER_KEY_PWD_FILE"]    || "/etc/ssl/private/server.pwd"
+  key_user    = ENV["SERVER_KEY_FILE_USER"]   || "root"
+  key_group   = ENV["SERVER_KEY_FILE_GROUP"]  || "root"
+  key_mode    = ENV["SERVER_KEY_FILE_MODE"]   || 440
 
-  subj = ENV["SERVER_CRT_SUBJECT"]  || "CN=#{ENV["CONTAINER_NAME"]}"
+  p12         = ENV["SERVER_P12_FILE"]        || "/etc/ssl/private/server.p12"
+  p12_pwd     = ENV["SERVER_KEY_PWD_FILE"]    || "/etc/ssl/private/server.pwd"
+  p12_user    = ENV["SERVER_KEY_FILE_USER"]   || "root"
+  p12_group   = ENV["SERVER_KEY_FILE_GROUP"]  || "root"
+  p12_mode    = ENV["SERVER_KEY_FILE_MODE"]   || 440
 
   ### CERTIFICATE ##############################################################
 
@@ -24,16 +32,16 @@ describe "Server certificate", :test => :server_cert do
     context "file" do
       subject { file(crt) }
       it { is_expected.to be_file }
-      it { is_expected.to be_mode(444) }
-      it { is_expected.to be_owned_by(user) }
-      it { is_expected.to be_grouped_into(group) }
+      it { is_expected.to be_mode(crt_mode) }
+      it { is_expected.to be_owned_by(crt_user) }
+      it { is_expected.to be_grouped_into(crt_group) }
     end
     context "certificate" do
       it { is_expected.to be_certificate }
       it { is_expected.to be_valid }
     end
-    its(:subject) { is_expected.to eq "/#{subj}" }
-    its(:issuer)  { is_expected.to eq "/CN=Simple CA" }
+    its(:subject) { is_expected.to eq "/#{crt_subj}" }
+    its(:issuer) { is_expected.to eq "/CN=Simple CA" }
     its(:validity_in_days) { is_expected.to be > 3650 }
     context "subject_alt_names" do
       if ! ENV["SERVER_CRT_HOST"].nil? then
@@ -51,13 +59,13 @@ describe "Server certificate", :test => :server_cert do
 
   ### PRIVATE_KEY_PASSPHRASE ###################################################
 
-  describe "X509 private key passphrase \"#{pwd}\"" do
+  describe "X509 private key passphrase \"#{key_pwd}\"" do
     context "file" do
-      subject { file(pwd) }
+      subject { file(key_pwd) }
       it { is_expected.to be_file }
-      it { is_expected.to be_mode(440) }
-      it { is_expected.to be_owned_by(user) }
-      it { is_expected.to be_grouped_into(group) }
+      it { is_expected.to be_mode(key_mode) }
+      it { is_expected.to be_owned_by(key_user) }
+      it { is_expected.to be_grouped_into(key_group) }
     end
   end
 
@@ -67,9 +75,9 @@ describe "Server certificate", :test => :server_cert do
     context "file" do
       subject { file(key) }
       it { is_expected.to be_file }
-      it { is_expected.to be_mode(440) }
-      it { is_expected.to be_owned_by(user) }
-      it { is_expected.to be_grouped_into(group) }
+      it { is_expected.to be_mode(key_mode) }
+      it { is_expected.to be_owned_by(key_user) }
+      it { is_expected.to be_grouped_into(key_group) }
     end
     context "key" do
       it { is_expected.to be_encrypted }
@@ -86,9 +94,9 @@ describe "Server certificate", :test => :server_cert do
     context "file" do
       subject { file(p12) }
       it { is_expected.to be_file }
-      it { is_expected.to be_mode(440) }
-      it { is_expected.to be_owned_by(user) }
-      it { is_expected.to be_grouped_into(group) }
+      it { is_expected.to be_mode(p12_mode) }
+      it { is_expected.to be_owned_by(p12_user) }
+      it { is_expected.to be_grouped_into(p12_group) }
     end
     context "keystore" do
       # TODO: Serverspec does not support PKCS12 keystores
@@ -124,7 +132,7 @@ describe "Server certificate", :test => :server_cert do
       # it { expect(subject.key).to be_encrypted_key }
       # it { expect(subject.key).to be_valid_key }
       # it { expect(subject.key).to have_matching_certificate }
-      subject { command("openssl pkcs12 -in #{p12} -passin file:#{pwd} -noout -info") }
+      subject { command("openssl pkcs12 -in #{p12} -passin file:#{key_pwd} -noout -info") }
       it "should be encrypted" do
         expect(subject.exit_status).to eq(0)
         expect(subject.stderr).to match(/^Shrouded Keybag: pbeWithSHA1And3-KeyTripleDES-CBC, Iteration 2048$/)
