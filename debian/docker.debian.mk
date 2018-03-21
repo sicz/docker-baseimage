79551426ad4cb00b342fa3a5aaf599a5671161af
Petr Řehoř <rx@rx.cz>
2018-03-21 19:48:30 +0100

    Use Debian slim base image

--- debian/docker.debian.mk
+++ debian/docker.debian.mk
@@ -1,7 +1,7 @@
 ### BASE_IMAGE #################################################################
 
 BASE_IMAGE_NAME		?= debian
-BASE_IMAGE_TAG		?= $(BASE_IMAGE_OS_VERSION)
+BASE_IMAGE_TAG		?= $(BASE_IMAGE_OS_VERSION)-slim
 
 BASE_IMAGE_OS_NAME	?= Debian
 BASE_IMAGE_OS_URL	?= https://debian.org
